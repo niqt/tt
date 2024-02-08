e6baf0888e039576f1ee7d3a815cf5d60913114c
Nicola De Filippo <nicola@nicoladefilippo.it>
2024-02-08 23:49:58 +0100

    Initial Commit

---
+++ tt/ttApp.swift
@@ -0,0 +1,17 @@
+//
+//  ttApp.swift
+//  tt
+//
+//  Created by nicola de filippo on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
